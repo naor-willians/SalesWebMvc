21ef98feb7f38266f62c377f850867e55c17d8cc
Naor Willians <naorwillians@hotmail.com>
2022-06-08 14:16:37 -0300

    Sales and Sellers

---
+++ SalesWebMvc/Models/SalesRecord.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using SalesWebMvc.Models.Enums;
+
+namespace SalesWebMvc.Models
+{
+    public class SalesRecord
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public double amount { get; set; }
+        public SalesStatus Status { get; set; }
+        public Seller Seller { get; set; }
+
+
+
+        public SalesRecord() { }
+
+        public SalesRecord(int id, DateTime date, double amount, SalesStatus status, Seller seller)
+        {
+            Id = id;
+            Date = date;
+            this.amount = amount;
+            Status = status;
+            Seller = seller;
+        }
+    }
+}
